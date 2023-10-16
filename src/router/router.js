56de9ac12a93f2f75ada9ae29a1554056e0bdb22
Miguel Castro <miguelcastro0207@gmail.com>
2023-10-16 18:30:31 -0400

    adding bert view

--- src/router/router.js
+++ src/router/router.js
@@ -7,4 +7,6 @@ const route = express.Router();
 route.get('/', mainController.hello);
 route.get('/seach', mainController.search);
 route.get('/seach/:query', mainController.search);
+route.get('/bert', mainController.bert);
+route.get('/bert/:query', mainController.bert);
 export default route;
